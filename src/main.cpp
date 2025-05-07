f9095d36c9224facb33e0eb6bee03a91d1655b6c
caolei <caolei6767@gmail.com>
2025-05-07 14:11:19 +0800

    base commit

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+int main(int /*argc*/, char* /*argv*/[]) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window =
+        glfwCreateWindow(800, 600, "Hello OpenGL", nullptr, nullptr);
+
+    if(window == nullptr) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if(glewInit() != GLEW_OK) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+    }
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
+
+    return 0;
+}
